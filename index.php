3250942dbcbff9fc3296ca5c73e5468526a2129c
Diogo Souza <diogo.souza@msn.com>
2017-07-13 09:32:41 -0400

    criado a Classe PageAdmin e tpl admin

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 require_once("vendor/autoload.php");
 
-use \Slim\Slim;
-use \Hcode\Page;
+use Slim\Slim;
+use Hcode\Page;
+use Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -12,9 +13,15 @@ $app->config('debug', true);
 $app->get('/', function() {
 
     $page = new Page();
-    
+
     $page->setTpl("index");
-    
+});
+
+$app->get('/admin', function() {
+
+    $page = new PageAdmin();
+
+    $page->setTpl("index");
 });
 
 $app->run();
