a9c57e03ee8edbb176ff04337f6941cd3192da17
dirsouza <diogo.souza@msn.com>
2017-08-05 22:27:28 -0400

    Criado lista de categorias e cadastro

---
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Hcode\Model;
+
+use Hcode\DB\Sql;
+use Hcode\Model;
+
+class Category extends Model {
+    
+    public static function listAll() {
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_categories ORDER BY idcategory");
+    }
+    
+    public function save() {
+        
+        $sql = new Sql();
+        
+        $results = $sql->select("CALL sp_categories_save(:descategory)", array(
+            ':descategory' => $this->getdescategory()
+        ));
+        
+        $this->setData($results[0]);
+        
+    }
+    
+}
